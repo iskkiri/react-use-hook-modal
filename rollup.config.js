6d2f5550d2bbbbbba8fa55bd11f79cf172af916d
iskkiri <yoon_1139@naver.com>
2024-10-09 18:02:48 +0900

    feat: remove rollup-plugin-dts & change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,30 +2,22 @@ import { defineConfig } from 'rollup';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
-import { dts } from 'rollup-plugin-dts';
 
 import pkg from './package.json' assert { type: 'json' };
 
-export default defineConfig([
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: pkg.module,
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-    plugins: [peerDepsExternal(), typescript({ tsconfig: './tsconfig.json' }), terser()],
-  },
-  {
-    input: 'dist/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'es' }],
-    plugins: [dts()],
-  },
-]);
+export default defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [peerDepsExternal(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+});
